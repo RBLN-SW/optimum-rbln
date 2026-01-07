202ecdcf249b378a1bdb1480dd0aaaaf10f29302
rebel-dkhong <dk.hong@rebellions.ai>
2026-01-07 19:01:59 +0900

    fix wrong reference

--- src/optimum/rbln/diffusers/models/transformers/transformer_cosmos.py
+++ src/optimum/rbln/diffusers/models/transformers/transformer_cosmos.py
@@ -329,7 +329,7 @@ class RBLNCosmosTransformer3DModel(RBLNModel):
             ),
         ]
 
-        if model_config.config.use_crossattn_projection or (
+        if model_config.use_crossattn_projection or (
             model_config.extra_pos_embed_type is None and rbln_config.is_v2w
         ):
             input_info.append(
