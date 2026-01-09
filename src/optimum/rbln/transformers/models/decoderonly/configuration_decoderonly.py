73c6e24cab920653a2815e95b8deeb8f416c287d
Jongho Choi <jongho.choi@rebellions.ai>
2026-01-09 12:25:44 +0900

    fix sliding

--- src/optimum/rbln/transformers/models/decoderonly/configuration_decoderonly.py
+++ src/optimum/rbln/transformers/models/decoderonly/configuration_decoderonly.py
@@ -353,7 +353,11 @@ class KVCacheMeta(RBLNSerializableConfigProtocol):
 
     @property
     def compile_shape(self):
-        return self.shape if not self.is_auto else [1, self.shape[1], self.shape[2], self.shape[3]]
+        return (
+            self.shape
+            if not self.is_auto or self.layer_type == "sliding_attention"
+            else [1, self.shape[1], self.shape[2], self.shape[3]]
+        )
 
     @property
     def num_blocks(self) -> int:
