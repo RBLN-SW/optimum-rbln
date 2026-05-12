e60d7d6f7dbadf213b96bf59533d3aa369ee8562
rebel-eunji <eunji.lee@rebellions.ai>
2026-05-12 09:33:15 +0900

    slicing

--- scripts/uv-sync.sh
+++ scripts/uv-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install every dependencies and ensure src/optimum/rbln/__version__.py is generated
-uv sync \
+uv sync --active \
     --frozen \
     --all-groups \
     --all-extras \
--- src/optimum/rbln/transformers/models/whisper/modeling_whisper.py
+++ src/optimum/rbln/transformers/models/whisper/modeling_whisper.py
@@ -318,7 +318,7 @@ class RBLNWhisperForConditionalGeneration(RBLNModel, RBLNWhisperGenerationMixin)
                     "cross_key_value_states",
                     [
                         model_config.decoder_layers * 2,
-                        rbln_config.batch_size,
+                        rbln_config.batch_size + 1,
                         model_config.decoder_attention_heads,
                         rbln_config.enc_max_seq_len,
                         model_config.d_model // model_config.decoder_attention_heads,
@@ -332,7 +332,7 @@ class RBLNWhisperForConditionalGeneration(RBLNModel, RBLNWhisperGenerationMixin)
                 (
                     f"self_key_value_states_{i}",
                     [
-                        rbln_config.batch_size,
+                        rbln_config.batch_size + 1,
                         model_config.decoder_attention_heads,
                         rbln_config.dec_max_seq_len,
                         model_config.d_model // model_config.encoder_attention_heads,
