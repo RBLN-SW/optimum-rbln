61fb678417de067e4e185b214c295933bb0b3e5e
rebel-eunji <eunji.lee@rebellions.ai>
2026-05-12 10:13:11 +0900

    revert: script

--- scripts/uv-sync.sh
+++ scripts/uv-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install every dependencies and ensure src/optimum/rbln/__version__.py is generated
-uv sync --active \
+uv sync \
     --frozen \
     --all-groups \
     --all-extras \
