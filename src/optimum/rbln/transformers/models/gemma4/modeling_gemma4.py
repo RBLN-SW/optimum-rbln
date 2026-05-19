c07c744590562bd1656bc56d5d10cb0d5c319673
rebel-dkhong <dk.hong@rebellions.ai>
2026-05-19 18:04:51 +0900

    remove comment

--- src/optimum/rbln/transformers/models/gemma4/modeling_gemma4.py
+++ src/optimum/rbln/transformers/models/gemma4/modeling_gemma4.py
@@ -466,7 +466,7 @@ class RBLNGemma4ForConditionalGeneration(RBLNModel, RBLNDecoderOnlyGenerationMix
     auto_model_class = AutoModelForImageTextToText
     _rbln_submodule_prefix = "model"
     _rbln_submodules = [
-        # {"name": "vision_tower"},
+        {"name": "vision_tower"},
         {"name": "language_model"},
     ]
     _supports_non_fp32 = True
