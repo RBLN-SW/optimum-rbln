1925034f9caf935b166ab7c0b3d0d8362f700ada
kblee <kblee@rebellions.ai>
2026-06-10 07:02:51 +0000

    fix encoder models

--- src/optimum/rbln/transformers/models/t5/modeling_t5.py
+++ src/optimum/rbln/transformers/models/t5/modeling_t5.py
@@ -17,6 +17,7 @@ from typing import TYPE_CHECKING, Any, Callable
 
 import torch
 from transformers import AutoModelForTextEncoding, T5EncoderModel, T5ForConditionalGeneration
+from transformers.modeling_attn_mask_utils import _prepare_4d_attention_mask
 from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
 
 from ...modeling_generic import RBLNTransformerEncoderForFeatureExtraction
@@ -38,6 +39,12 @@ class T5EncoderWrapper(torch.nn.Module):
 
     def forward(self, *args, **kwargs):
         kwargs.pop("return_dict", None)
+        # TODO: make this to use `create_bidirectional_mask` in transformers v5
+        args = list(args)
+        if len(args) > 1 and torch.is_tensor(args[1]) and args[1].dim() == 2:
+            args[1] = _prepare_4d_attention_mask(args[1], torch.float32)
+        if "attention_mask" in kwargs and kwargs["attention_mask"] is not None and kwargs["attention_mask"].dim() == 2:
+            kwargs["attention_mask"] = _prepare_4d_attention_mask(kwargs["attention_mask"], torch.float32)
         return self.model(*args, **kwargs, return_dict=False)
 
 
