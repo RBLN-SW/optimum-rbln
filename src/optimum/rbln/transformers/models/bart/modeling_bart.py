9adfc2b78ae9542469e23f5748e0c43c16088b58
rebel-dkhong <dk.hong@rebellions.ai>
2026-08-13 11:20:05 +0900

    roll back _supports_non_fp32

--- src/optimum/rbln/transformers/models/bart/modeling_bart.py
+++ src/optimum/rbln/transformers/models/bart/modeling_bart.py
@@ -68,7 +68,6 @@ class RBLNBartForConditionalGeneration(RBLNModelForSeq2SeqLM):
     """
 
     support_causal_attn = True
-    _supports_non_fp32 = False
 
     @classmethod
     def _wrap_model_if_needed(self, model: PreTrainedModel, rbln_config: RBLNBartForConditionalGenerationConfig):
